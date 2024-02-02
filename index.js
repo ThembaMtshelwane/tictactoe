8f322f5df91e83435f5f8755fce40658f80bc66e
Themba Mtshelwane <Thembamm3@gmail.com>
2024-02-02 12:56:21 +0200

    restart game

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const blocks = document.getElementsByClassName('block')
 const selectElement = document.querySelector('select')
+const restartButton = document.getElementById('restart')
+
 let selectedToken = selectElement.value
 
 selectElement.addEventListener('change', () => {
   selectedToken = selectElement.value
+})
+
+restartButton.addEventListener('click', () => {
+  const allBlocksFilled = Array.from(blocks).every(
+    (block) => block.textContent !== ''
+  )
+  if (allBlocksFilled) {
+    Array.from(blocks).forEach((block) => {
+      block.textContent = ''
+    })
+  }
 })
 
 Array.from(blocks).forEach((block) => {
